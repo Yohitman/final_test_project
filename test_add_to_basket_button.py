4b99c630cb6e0819c6203e59f775eba90a93074b
Yohitman <pav161195@yandex.ru>
2019-12-09 20:07:14 +0300

    update name of the test

--- test_add_to_basket_button.py
+++ test_add_to_basket_button.py
@@ -1,7 +1,7 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_button_add_to_basket(browser):
     browser.get(link)
     is_button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
     try:
